5de82b9de48822768298de1a94cad44695b2ae27
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-05-23 10:04:07 -0400

    Create seed lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lines = [
+    "Nestlé",
+    "Purina",
+    "Food",
+    "Garoto"
+    ]
+
+    lines.each do |line|
+        Line.find_or_create_by(line_description: line)
+    end
