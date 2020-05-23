e93198f73abeca2a9c4cdf50ebf321adec507fd2
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-05-23 09:41:11 -0400

    Config friendly_id and pg_search

--- app/controllers/product_search_controller.rb
+++ app/controllers/product_search_controller.rb
@@ -1,8 +1,7 @@
 class ProductSearchController < ApplicationController
 
-
   def search
-    @products = Product.limit(9).releases_home
+    @products = Product.all
     @groups = Group.order(:group_description)
     @products = Product.search(search_params[:q])
   end
